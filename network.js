7f62bbcc294ec9051966898654d928ab400bd4e5
Fumeng Yang <yang.form@gmail.com>
2016-11-28 22:13:59 -0500

    minor tweaks and cleaning

--- network.js
+++ network.js
@@ -1,63 +1,3 @@
-
-
-/* accepts parameters
- * r  Object = {r:x, g:y, b:z}
- * OR 
- * r, g, b
-*/
-function RGBtoHSV(r, g, b) {
-    if (arguments.length === 1) {
-        g = r.g, b = r.b, r = r.r;
-    }
-    var max = Math.max(r, g, b), min = Math.min(r, g, b),
-        d = max - min,
-        h,
-        s = (max === 0 ? 0 : d / max),
-        v = max / 255;
-
-    switch (max) {
-        case min: h = 0; break;
-        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
-        case g: h = (b - r) + d * 2; h /= 6 * d; break;
-        case b: h = (r - g) + d * 4; h /= 6 * d; break;
-    }
-
-    return {
-        h: h,
-        s: s,
-        v: v
-    }
-}
- /* accepts parameters
- * h  Object = {h:x, s:y, v:z}
- * OR 
- * h, s, v
-*/
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (arguments.length === 1) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return {
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255)
-    };
-}
-
 /***********************************************************************************/
 /********************************    Edges     *************************************/
 /***********************************************************************************/
@@ -119,6 +59,7 @@ var Node = function(atom, aid, name,
     this.atom.energy = this.energy
     this.radius = props.radius
 
+    // for reseting the system
     this.atom_ = this.atom.clone()
     this.position_ = this.position.clone()
     this.oldposition_ = this.position_.clone()
